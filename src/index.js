b3bda7264253183b0153d11317b1a7f46ba608bc
Kasey <kasey.murdoch@yahoo.com>
2022-04-05 22:11:14 -0700

    Todo removal functional

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // This file will be used to manipulate DOM
 
 import './style.css';
-import { projectFactory } from './todo';
+import { projectFactory, removeTodo } from './todo';
 import { todoFactory } from './todo';
 import { addProject } from './todo';
 import { addTodo } from './todo';
+import { deleteTodo } from './todo';
 
 const content = document.querySelector(`.content`)
 const addProjBtn = document.querySelector(`.add-project`)
@@ -13,28 +14,47 @@ const defaultProject = projectFactory(`Starting List`)
 defaultProject.itemArray = []
 let projectArray = [defaultProject]
 
+// const domPost = (indexType, index, textContent, container, key) => {
+//     const div = document.createElement(`div`)
+//     div.setAttribute(`${indexType}-index`, index)
+//     div.classList.add(`${indexType}-${key}`)
+//     div.textContent = textContent
+//     container.appendChild(div)
+// }
+
 const display = () => {
     content.innerHTML = ``
     console.log(projectArray)
 
     projectArray.forEach((e, index) => {
+        const projectContainer = document.createElement(`div`)
+        projectContainer.setAttribute(`project-index`, index)
+        content.appendChild(projectContainer)
+
         const div = document.createElement(`div`)
         div.setAttribute(`project-index`, index)
         div.classList.add(`project-title`)
         div.textContent = e.title
-        content.appendChild(div)
+        projectContainer.appendChild(div)
 
         const itemContainer = document.createElement(`div`)
         itemContainer.classList.add(`item-container`)
-        content.appendChild(itemContainer)
+        projectContainer.appendChild(itemContainer)
 
         const button = document.createElement(`button`)
         button.setAttribute(`project-index`, index)
         button.classList.add(`add-item`)
         button.textContent = `+`
-        content.appendChild(button)
+        projectContainer.appendChild(button)
 
+        // Iterates through each project object's itemArray which contains the project's todos and then posts each todo property to the webpage.
         e.itemArray.forEach((e, index) => {
+            // domPost(`item`, index, e.title, itemContainer, )
+            // domPost(`item`, index, e.description, itemContainer)
+            // domPost(`item`, index, e.dueDate, itemContainer)
+            // domPost(`item`, index, e.priority, itemContainer)
+            // domPost(`item`, index, e.notes, itemContainer)
+
             const itemTitle = document.createElement(`div`)
             itemTitle.setAttribute(`item-index`, index)
             itemTitle.classList.add(`item-title`)
@@ -43,27 +63,33 @@ const display = () => {
 
             const itemDesc = document.createElement(`div`)
             itemDesc.setAttribute(`item-index`, index)
-            itemDesc.classList.add(`item-title`)
+            itemDesc.classList.add(`item-description`)
             itemDesc.textContent = `Description: ${e.description}`
             itemContainer.appendChild(itemDesc)
 
             const itemDue = document.createElement(`div`)
             itemDue.setAttribute(`item-index`, index)
-            itemDue.classList.add(`item-title`)
+            itemDue.classList.add(`item-dueDate`)
             itemDue.textContent = `Due Date: ${e.dueDate}`
             itemContainer.appendChild(itemDue)
 
             const itemPriority = document.createElement(`div`)
             itemPriority.setAttribute(`item-index`, index)
-            itemPriority.classList.add(`item-title`)
+            itemPriority.classList.add(`item-priority`)
             itemPriority.textContent = `Priority: ${e.priority}`
             itemContainer.appendChild(itemPriority)
 
             const itemNotes = document.createElement(`div`)
             itemNotes.setAttribute(`item-index`, index)
-            itemNotes.classList.add(`item-title`)
+            itemNotes.classList.add(`item-notes`)
             itemNotes.textContent = `Notes: ${e.notes}`
             itemContainer.appendChild(itemNotes)
+
+            const itemDelete = document.createElement(`button`)
+            itemDelete.setAttribute(`item-index`, index)
+            itemDelete.classList.add(`item-delete`)
+            itemDelete.textContent = `Delete`
+            itemContainer.appendChild(itemDelete)
         })
     })
 }
@@ -77,6 +103,13 @@ addProjBtn.addEventListener(`click`, () => {
 document.addEventListener(`click`, (event) => {
     if (event.target.classList.contains(`add-item`)) {
         addTodo(event, projectArray)
+        display()
+    }
+})
+
+document.addEventListener(`click`, (event) => {
+    if (event.target.classList.contains(`item-delete`)) {
+        deleteTodo(event, projectArray)
         display()
     }
 })
--- src/todo.js
+++ src/todo.js
@@ -32,3 +32,16 @@ export const addTodo = (event, projectArray) => {
     selectObject.itemArray.push(newItem)
     console.log(selectObject.itemArray)
 }
+
+export const deleteTodo = (event, projectArray) => {
+     // We go up parent elements twice from the delete button to select the project index div and get its correlated project index position
+     const projectIndex = event.target.parentElement.parentElement.getAttribute(`project-index`)
+     // We select the delete button to get its correlated item index posiition
+     const itemIndex = event.target.getAttribute(`item-index`)
+     // Then we select the correct project object inside the project array
+     const selectedProject = projectArray[projectIndex]
+     // Finally we access the itemArray inside of the correct project and splice out the Todo item
+     selectedProject.itemArray.splice(itemIndex, 1)
+     console.log(projectIndex)
+     console.log(itemIndex)
+}
