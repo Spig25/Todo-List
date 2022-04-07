7946c8cdbf268fe9ad2b2de2712e41a6b671a794
Kasey <kasey.murdoch@yahoo.com>
2022-04-06 21:29:38 -0700

    Updating dist

--- src/index.js
+++ src/index.js
@@ -56,46 +56,67 @@ export const display = () => {
             itemTitle.textContent = e.title
             itemContainer.appendChild(itemTitle)
 
-            const itemDesc = document.createElement(`div`)
-            itemDesc.setAttribute(`item-index`, index)
-            itemDesc.classList.add(`item-description`)
-            itemDesc.textContent = `Description: ${e.description}`
-            itemContainer.appendChild(itemDesc)
+            if (e.description !== ``) {
+                const itemDesc = document.createElement(`div`)
+                itemDesc.setAttribute(`item-index`, index)
+                itemDesc.classList.add(`item-description`)
+                itemDesc.textContent = `Description: ${e.description}`
+                itemContainer.appendChild(itemDesc)
+            }
+            
+            if (e.dueDate !== ``) {
+                const itemDue = document.createElement(`div`)
+                itemDue.setAttribute(`item-index`, index)
+                itemDue.classList.add(`item-dueDate`)
+                itemDue.textContent = `Due Date: ${e.dueDate}`
+                itemContainer.appendChild(itemDue)
+            }
 
-            const itemDue = document.createElement(`div`)
-            itemDue.setAttribute(`item-index`, index)
-            itemDue.classList.add(`item-dueDate`)
-            itemDue.textContent = `Due Date: ${e.dueDate}`
-            itemContainer.appendChild(itemDue)
+            if (e.priority !== ``) {
+                const itemPriority = document.createElement(`div`)
+                itemPriority.setAttribute(`item-index`, index)
+                itemPriority.classList.add(`item-priority`)
+                itemPriority.textContent = `Priority: ${e.priority}`
+                itemContainer.appendChild(itemPriority)
+            }
 
-            const itemPriority = document.createElement(`div`)
-            itemPriority.setAttribute(`item-index`, index)
-            itemPriority.classList.add(`item-priority`)
-            itemPriority.textContent = `Priority: ${e.priority}`
-            itemContainer.appendChild(itemPriority)
-
-            const itemNotes = document.createElement(`div`)
-            itemNotes.setAttribute(`item-index`, index)
-            itemNotes.classList.add(`item-notes`)
-            itemNotes.textContent = `Notes: ${e.notes}`
-            itemContainer.appendChild(itemNotes)
+            if (e.notes !== ``) {
+                const itemNotes = document.createElement(`div`)
+                itemNotes.setAttribute(`item-index`, index)
+                itemNotes.classList.add(`item-notes`)
+                itemNotes.textContent = `Notes: ${e.notes}`
+                itemContainer.appendChild(itemNotes)
+            }
 
             const itemDelete = document.createElement(`button`)
             itemDelete.setAttribute(`item-index`, index)
             itemDelete.classList.add(`item-delete`)
-            itemDelete.textContent = `Delete`
+            itemDelete.textContent = `Delete Item`
             itemContainer.appendChild(itemDelete)
         })
     })
 }
 display()
 
+// When project/item form is submitted/cancelled out we run this function. Created this so that if future form cancel/submit logic needs to be changed it can all be done right here
+const formExit = (formContainer, addOrRemove) => {
+    if (addOrRemove === `add`) {
+        formContainer.classList.add(`show`)
+    }
+    if (addOrRemove === `remove`) {
+        formContainer.classList.remove(`show`)
+        if (formContainer === itemFormContainer) {
+            document.querySelector(`.item-submit`).remove()
+        }
+    }
+}
+
 document.querySelector(`.add-project`).addEventListener(`click`, () => {
-    projectFormContainer.classList.add(`show`)
+    formExit(projectFormContainer, `add`)
 })
 document.querySelector(`.project-submit`).addEventListener(`click`, () => {
     addProject(projectArray)
-    projectFormContainer.classList.remove(`show`)
+    formExit(projectFormContainer, `remove`)
 })
 document.addEventListener(`click`, (event) => {
     if (event.target.classList.contains(`project-delete`)) {
@@ -103,11 +124,14 @@ document.addEventListener(`click`, (event) => {
         display()
     }
 })
+document.querySelector(`.project-cancel`).addEventListener(`click`, () => {
+    formExit(projectFormContainer, `remove`)
+})
 
 // Adding item takes far more code than adding project because there is only one add project button vs many add item buttons. We need to be able to tell addTodo() which project we are adding an item to
 document.addEventListener(`click`, (event) => {
     if (event.target.classList.contains(`add-item`)) {
-        itemFormContainer.classList.add(`show`)
+        formExit(itemFormContainer, `add`)
         const button = document.createElement(`button`)
         button.classList.add(`item-submit`)
         button.textContent = `Submit`
@@ -123,8 +147,7 @@ document.addEventListener(`click`, (event) => {
         const projectIndex = button.getAttribute(`project-index`)
         // We use the previously created button value to tell addTodo() which project we are adding a todo item to.
         addTodo(projectIndex, projectArray)
-        itemFormContainer.classList.remove(`show`)
-        button.remove()
+        formExit(itemFormContainer, `remove`)
     }
 })
 document.addEventListener(`click`, (event) => {
@@ -133,3 +156,7 @@ document.addEventListener(`click`, (event) => {
         display()
     }
 })
+document.querySelector(`.item-cancel`).addEventListener(`click`, () => {
+    formExit(itemFormContainer, `remove`)
+})
+
